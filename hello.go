ac6b8fd3fa6f9f449ecd8587087f36b3854c4bd0
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-07-27 22:43:38 -0300

    Hacking in go

---
+++ hello.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf(getName("Augusto"));
+	testing()
+	loopTest()
+}
+
+func getName(name string) string {
+	return "Hello, " + name + "!"
+}
+
+func testing() {
+	fmt.Print("\n")
+	slice := []int{12,1231,12312}
+	fmt.Print(slice)
+
+	fmt.Print("\n")
+	slice2 := []byte("Hello")
+	fmt.Print(slice2)
+}
+
+func loopTest() {
+   fmt.Print("\n")
+   entry := []string{"Jack","John","Jones"}
+   for i, val := range entry {
+     fmt.Printf("At position %d, the character %s is present\n", i, val)
+   }
+}
