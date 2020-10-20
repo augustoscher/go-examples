7a41e7e2de3e496480592069ee5847ca339ef317
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-10-19 22:52:57 -0300

    Control structures

--- curso-go/2-controls/ifs/ifelse.go
+++ curso-go/2-controls/ifs/ifelse.go
@@ -2,16 +2,41 @@ package main
 
 import "fmt"
 
-func printResult(grade float64) {
+func printResult(grade float64) string {
 	if grade >= 7 {
-		fmt.Println("Approved: ", grade)
+		return "Approved"
 	} else {
-		fmt.Println("Repproved: ", grade)
+		return "Repproved"
+	}
+}
+
+func gradeToConcept(grade float64) string {
+	if grade >= 9 && grade <= 10 {
+		return "A"
+	} else if grade >= 8 && grade < 9 {
+		return "B"
+	} else if grade >= 5 && grade < 8 {
+		return "C"
+	} else if grade < 5 {
+		return "D"
+	} else {
+		return "E"
 	}
 }
 
 func main() {
-	printResult(7.1)
-	printResult(7)
-	printResult(6.9)
+	grade := 9.0
+	fmt.Printf("Grade: %f - Status: %s - Concept: %s\n", grade, printResult(grade), gradeToConcept(grade))
+
+	grade = 8
+	fmt.Printf("Grade: %f - Status: %s - Concept: %s\n", grade, printResult(grade), gradeToConcept(grade))
+
+	grade = 5
+	fmt.Printf("Grade: %f - Status: %s - Concept: %s\n", grade, printResult(grade), gradeToConcept(grade))
+
+	grade = 4
+	fmt.Printf("Grade: %f - Status: %s - Concept: %s\n", grade, printResult(grade), gradeToConcept(grade))
+
+	grade = 11
+	fmt.Printf("Grade: %f - Status: %s - Concept: %s\n", grade, printResult(grade), gradeToConcept(grade))
 }
