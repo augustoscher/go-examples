f1f25f820ca0112465e0610058357b6b263c45ef
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-10-20 23:24:22 -0300

    Arrays, Slices, Map

--- curso-go/3-array-slice-map/array/array.go
+++ curso-go/3-array-slice-map/array/array.go
@@ -5,5 +5,16 @@ import "fmt"
 func main() {
 	// arrays são homogêneos (mesmo tipo de dado dentro dele) e estática (fixo)
 	var grades [3]float64
+	fmt.Println(grades) // [0, 0, 0]
+
+	grades[0], grades[1], grades[2] = 9.8, 7.4, 10.0
 	fmt.Println(grades)
+
+	total := 0.0
+	for i := 0; i < len(grades); i++ {
+		total += grades[i]
+	}
+
+	avg := total / float64(len(grades))
+	fmt.Printf("Méida é: %.2f\n", avg)
 }
