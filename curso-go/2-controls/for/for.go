0f3805d0f848ea8c317036d50de1e6a15d7f2b95
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-10-20 22:23:08 -0300

    Loop

--- curso-go/2-controls/for/for.go
+++ curso-go/2-controls/for/for.go
@@ -1,13 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	// while way
+	fmt.Println("while way")
 	i := 1
 	for i <= 10 {
 		fmt.Printf("i: %d\n", i)
 		i++
+	}
+
+	fmt.Println("")
+	fmt.Println("Tradicional")
+	for i := 0; i <= 20; i += 2 {
+		fmt.Printf("%d ", i)
+	}
+	fmt.Println("")
+
+	fmt.Println("")
+	fmt.Println("Mixing")
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Printf("Numero %d é par\n", i)
+		} else {
+			fmt.Printf("Numero %d é impar\n", i)
+		}
+	}
+
+	fmt.Println("")
+	fmt.Println("Loop infinito")
+	for {
+		fmt.Println("Oi...")
+		time.Sleep(time.Second)
 	}
 
 }
