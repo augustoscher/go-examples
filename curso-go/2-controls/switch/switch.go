9446317ef7484770ed3198d227835e2f58bbb710
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-10-20 23:13:43 -0300

    Switch

--- curso-go/2-controls/switch/switch.go
+++ curso-go/2-controls/switch/switch.go
@@ -25,10 +25,7 @@ func gradeToConcept(grade float64) string {
 	}
 }
 
-func main() {
-	fmt.Println(gradeToConcept(10))
-	fmt.Println(gradeToConcept(5))
-
+func getFromTime() {
 	t := time.Now()
 	switch {
 	// entra na primeira opção que der verdadeiro
@@ -39,5 +36,32 @@ func main() {
 	default:
 		fmt.Println("Boa noite!")
 	}
+}
 
+func getValueFromType(i interface{}) string {
+	switch i.(type) {
+	case int:
+		return "integer"
+	case float32, float64:
+		return "real"
+	case string:
+		return "string"
+	case func():
+		return "function"
+	default:
+		return "unknow"
+	}
+}
+
+func main() {
+	fmt.Println(gradeToConcept(10))
+	fmt.Println(gradeToConcept(5))
+
+	getFromTime()
+
+	fmt.Println(getValueFromType(2.9))
+	fmt.Println(getValueFromType(1))
+	fmt.Println(getValueFromType("oi"))
+	fmt.Println(getValueFromType(func() {}))
+	fmt.Println(getValueFromType(time.Now()))
 }
