d249d2c8a5b49c417a4181f26ec2bec8200e5780
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-10-22 15:14:45 -0300

    Arrays, Slices, Map

--- curso-go/3-array-slice-map/slice/slice.go
+++ curso-go/3-array-slice-map/slice/slice.go
@@ -1,6 +1,14 @@
 package main
 
-func main() {
-	a1 := [3]int{1, 2, 3}
+import "fmt"
 
+// slice: flexibiliza o uso do array
+// array tem tamanho fixo definido
+// slice tem um tamanho variável
+
+func main() {
+	array1 := [3]int{1, 2, 3}
+	slice1 := []int{1, 2, 3}
+
+	fmt.Println(array1, slice1)
 }
