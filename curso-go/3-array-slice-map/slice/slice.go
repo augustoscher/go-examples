b025703bc9ffcb141e59925284b6c08b41e79dc5
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-10-22 15:57:56 -0300

    Arrays, Slices, Map

--- curso-go/3-array-slice-map/slice/slice.go
+++ curso-go/3-array-slice-map/slice/slice.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
-// slice: flexibiliza o uso do array
+// slice: Representa um pedaço de um array
+// flexibiliza o uso do array
 // array tem tamanho fixo definido
 // slice tem um tamanho variável
 
@@ -11,4 +15,19 @@ func main() {
 	slice1 := []int{1, 2, 3}
 
 	fmt.Println(array1, slice1)
+	fmt.Println(reflect.TypeOf(array1), reflect.TypeOf(slice1))
+
+	array2 := [5]int{5, 4, 3, 2, 1}
+	slice2 := array2[1:3]
+	fmt.Println(slice2) // 4, 3 pois o ultimo elemento não é incluído.
+
+	s3 := array2[:3]
+	fmt.Println(s3) //5, 4, 3
+
+	s4 := array2[3:]
+	fmt.Println(s4) //2, 1
+
+	sliceOfSlice := s3[:1]
+	fmt.Println(sliceOfSlice) //5
+
 }
