28d9b30c6c015e1ed05ae6abc2b15a05444044be
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-10-26 00:01:30 -0300

    Functions

---
+++ curso-go/4-funcs/basics/basics.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func f1() {
+	fmt.Println("F1")
+}
+
+func f2(p1 string, p2 string) {
+	fmt.Printf("F2 %s %s \n", p1, p2)
+}
+
+func f3() string {
+	return "F3"
+}
+
+func f4(p1, p2 string, p3 int) string {
+	return fmt.Sprintf("F4 %s - %s - %d", p1, p2, p3)
+}
+
+func main() {
+	f1()
+	f2("Oi,", "Tudo bem?")
+	fmt.Println(f3())
+	fmt.Println(f4("Oi,", "Tudo bem?", 1392234))
+}
