1f42c2336cfb9a9cf69c80cdc826f80381dd7ad2
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-10-26 23:24:30 -0300

    Functions

--- curso-go/4-funcs/basics/basics.go
+++ curso-go/4-funcs/basics/basics.go
@@ -20,9 +20,24 @@ func f4(p1, p2 string, p3 int) string {
 	return fmt.Sprintf("F4 %s - %s - %d", p1, p2, p3)
 }
 
+// Returning multiple values
+func f5() (string, int64) {
+	return "One", 2
+}
+
 func main() {
 	f1()
 	f2("Oi,", "Tudo bem?")
 	fmt.Println(f3())
 	fmt.Println(f4("Oi,", "Tudo bem?", 1392234))
+	message, value := f5()
+	fmt.Printf("Message: %s - Value: %d\n", message, value)
+
+	// valid:
+	// _, r5 := f5() ignoring first return value
+	// r5, _ := f5() ignoring second return value
+	// f5() not expect for returns
+
+	// not valid:
+	// r5 := f5() not same returns are mapped
 }
