a7519af832f08499de15e431a87fbb4dac64e82c
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-10-29 10:00:10 -0300

    Functions

--- curso-go/4-funcs/closure/closure.go
+++ curso-go/4-funcs/closure/closure.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+// func que retorna uma outra func
+func closure() func() {
+	x := 10
+	var fn = func() {
+		fmt.Println(x)
+	}
+	return fn
+}
+
+func main() {
+	x := 20
+	fmt.Println(x)
+	fn := closure()
+	fn() //print x of lin 7
 }
