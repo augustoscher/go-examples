0f74940c1186ce7212b43dde9e116848b79a434b
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-10-29 00:39:37 -0300

    Functions

--- curso-go/4-funcs/reduce/reduce.go
+++ curso-go/4-funcs/reduce/reduce.go
@@ -13,8 +13,15 @@ func max(a, b float64) float64 {
 	return b
 }
 
+func min(a, b float64) float64 {
+	if a <= b {
+		return a
+	}
+	return b
+}
+
 func reduce(fn func(float64, float64) float64, numbers ...float64) float64 {
-	result := 0.0
+	result := numbers[0]
 	for _, number := range numbers {
 		result = fn(result, number)
 	}
@@ -22,31 +29,13 @@ func reduce(fn func(float64, float64) float64, numbers ...float64) float64 {
 }
 
 func avg(numbers ...float64) float64 {
-	// total := 0.0
-	// for _, number := range numbers {
-	// 	total += number
-	// }
-	fmt.Println(reduce(max, numbers...))
-
 	total := reduce(sum, numbers...)
 	return total / float64(len(numbers))
 }
 
-func printApproved(approved ...string) {
-	fmt.Println("Approved:")
-	for _, value := range approved {
-		fmt.Println(value)
-	}
-}
-
 func main() {
-	fmt.Printf("Avg: %.2f\n", avg(5.0, 7.0, 5.0))
-
-	name := []string{
-		"John",
-		"Tom",
-		"Hana",
-	}
-	// spread slice in variadic
-	printApproved(name...)
+	numbers := []float64{5.0, 7.5, 3.2}
+	fmt.Printf("Avg: %.2f\n", avg(numbers...))
+	fmt.Printf("Max: %.2f\n", reduce(max, numbers...))
+	fmt.Printf("Min: %.2f\n", reduce(min, numbers...))
 }
