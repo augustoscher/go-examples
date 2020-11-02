570f899fa9e29a9c83178cda06b9435c24bcfe75
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-11-02 14:02:22 -0300

    Functions

--- curso-go/4-funcs/pointer/pointers.go
+++ curso-go/4-funcs/pointer/pointers.go
@@ -1,13 +1,28 @@
 package main
 
+import "fmt"
+
 func inc1(n int) {
 	n++
 }
 
+//Ponteiro é representado por un *
 func inc2(n *int) {
+	// * usado para obter o valor ao qual o ponteiro aponta
+	// *n retorna o valor e então incrementamos
+	*n++
 
 }
 
 func main() {
+	n := 1
 
+	//passando por valor (valor de n não é alterado)
+	inc1(n)
+	fmt.Println(n) //1
+
+	//passando o endereço de memória (ponteiro) para a inc2
+	//passando por referência, onde o valor de n será alterado
+	inc2(&n)
+	fmt.Println(n) //2
 }
