b28ca6ea3f3483a51d06c23af47bc70a4ccfe4f4
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-11-06 22:22:04 -0300

    Json to Struct

--- curso-go/5-types/json/json.go
+++ curso-go/5-types/json/json.go
@@ -13,10 +13,19 @@ type Product struct {
 }
 
 func main() {
-	// struct to json
+	// 1. struct to json
 	p1 := Product{1, "Macbook", 1999.90, []string{"i5", "8gb ram"}}
 
 	//retorna um slice de bytes e o erro
 	p1Json, _ := json.Marshal(p1)
 	fmt.Println(string(p1Json))
+
+	// 2. json to struct
+	var p2 Product
+	jsonString := `{"id": 2,"name":"Macbook Pro","price":2999.9,"tags":["i7","16gb ram"]}`
+
+	// Passa um slice de bytes com base na string json e a referência do objeto p2
+	json.Unmarshal([]byte(jsonString), &p2)
+	fmt.Println(p2)
+	fmt.Println(p2.Tags[1])
 }
