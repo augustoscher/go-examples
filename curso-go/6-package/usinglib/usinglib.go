6d1e26a886fc615456c4eb3e38179de810c39a1a
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-11-08 14:21:52 -0300

    Using external lib

---
+++ curso-go/6-package/usinglib/usinglib.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/augustoscher/goarea"
+)
+
+func main() {
+	// Installing package:
+	// go get -u github.com/augustoscher/goarea
+
+	// Just using area.CircunferenceArea go imports package created on gopath folder
+	// Is not necessary compile the package file.
+	// When we run it at the first time, he's automatically compiled
+	fmt.Println(goarea.CircunferenceArea(6.0))
+
+	fmt.Println(goarea.RectangleArea(3.0, 5.4))
+
+	// Not visible
+	// fmt.Println(goarea._EquilateralTriangleArea(3.0, 4.0))
+}
