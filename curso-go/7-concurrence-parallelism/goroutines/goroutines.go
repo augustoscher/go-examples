6468e91f55dc0fb310952968aaffbbfd85ba761a
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-11-23 22:26:39 -0300

    Concurrence vs Parallelism

--- curso-go/7-concurrence-parallelism/goroutines/goroutines.go
+++ curso-go/7-concurrence-parallelism/goroutines/goroutines.go
@@ -13,5 +13,21 @@ func speak(person, text string, qtd int) {
 }
 
 func main() {
-	speak("Augusto", "Oi", 3)
+	// sequencial
+	// speak("Joao", "Oi, tudo bem?", 3)
+	// speak("Maria", "Oi, tudo certo e vc?", 1)
+
+	//goroutines
+	go speak("Joao", "Hey..", 500)
+	go speak("Maria", "Ohh", 550)
+
+	//A thread principal deve estar funcionando para as
+	//goroutines executarem.
+	time.Sleep(time.Second * 100)
+
+	// Caso fizessemos assim:
+	// go speak("Joao", "Hey..", 10) //goroutine
+	// speak("Maria", "Ohh", 5)      //thread principal
+	// Não daria tempo suficiente para Joao "falar todas as vezes"
+
 }
