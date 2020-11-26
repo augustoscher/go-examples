ea3d6b1ad83ce9690bae25105d1b2c8287c2afad
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-11-26 13:50:10 -0300

    Concurrence vs Parallelism - Buffered Channel

---
+++ curso-go/7-concurrence-parallelism/buffered-channel/buffered-channel.go
@@ -0,0 +1,10 @@
+package main
+
+// A escrita de dados é bufferizada.
+// Operação não bloqueante.
+// Quando temos um buffer de 10 posições, só vai bloquear depois que as 10 tiverem ocupadas.
+// Quando mandar a 11º informação, vai ficar esperando alguem ler pelo menos 1 info para escrever a proxima.
+
+func main() {
+
+}
