f8d371e464504a3a429f0249882dbdaf0fbf8c7e
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-11-25 22:30:44 -0300

    Concurrence vs Parallelism - Channel

---
+++ curso-go/7-concurrence-parallelism/deadlock/deadlock.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func doSomething(c chan int) {
+	time.Sleep(time.Second)
+	c <- 1 //operação bloqueante
+	fmt.Println("Só será printado depois que for lido...")
+}
+
+func main() {
+	c := make(chan int) //canal sem buffer
+
+	go doSomething(c)
+
+	fmt.Println(<-c) //operacao bloqueante
+	fmt.Println("Lido")
+	fmt.Println(<-c) //deadlock
+	fmt.Println("Não imprime pois da erro na linha acima")
+}
