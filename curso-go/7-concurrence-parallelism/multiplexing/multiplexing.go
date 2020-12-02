fc4454c09fe845cf47e5599ef903f9bc984bd735
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-12-01 22:57:27 -0300

    Concurrence vs Parallelism - Multiplexing

--- curso-go/7-concurrence-parallelism/multiplexing/multiplexing.go
+++ curso-go/7-concurrence-parallelism/multiplexing/multiplexing.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/augustoscher/goarea/html"
+)
+
+// precisa atualizar o pachage:
+// go get -u github.com/augustoscher/goarea
+
 // Multiplexing é uma tecnica onde pegamos dados de duas ou mais fontes
 // e escrevemos no mesmo canal.
 // Ou seja, temos dois channels gerando dados e juntamos em um único.
@@ -11,6 +20,22 @@ func send(origin <-chan string, destination chan string) {
 	}
 }
 
-func main() {
+// Responsável por misturar mensagens em um canal.
+// Recebe dois canais somente leitura
+func multiplex(entry1, entry2 <-chan string) <-chan string {
+	c := make(chan string)
+	go send(entry1, c)
+	go send(entry2, c)
+	return c
+}
 
+func main() {
+	c := multiplex(
+		//cada chamada das funcs abaixo retornam um canal
+		html.Title("http://augustoscher.com", "https://www.google.com.br"),
+		html.Title("https://amazon.com", "https://www.youtube.com"),
+	)
+
+	fmt.Println("First: ", <-c, " | Second: ", <-c)
+	fmt.Println("Third: ", <-c, " | Fourth: ", <-c)
 }
