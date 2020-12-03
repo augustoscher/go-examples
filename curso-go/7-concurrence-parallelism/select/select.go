05ef85b0eec2b1398afd9f2560025b11d1ba98ba
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-12-03 09:35:58 -0300

    Concurrence vs Parallelism - Select

--- curso-go/7-concurrence-parallelism/select/select.go
+++ curso-go/7-concurrence-parallelism/select/select.go
@@ -1,9 +1,33 @@
 package main
 
-func fastest() {
+import (
+	"fmt"
+	"time"
 
+	"github.com/augustoscher/goarea/html"
+)
+
+func fastest(url1, url2, url3 string) string {
+	c1 := html.Title(url1)
+	c2 := html.Title(url2)
+	c3 := html.Title(url3)
+
+	// Estrutura de controle específica para concorrência
+	select {
+	case t1 := <-c1:
+		return t1
+	case t2 := <-c2:
+		return t2
+	case t3 := <-c3:
+		return t3
+	case <-time.After(1000 * time.Millisecond):
+		return "Todos perdera!"
+		// default:
+		// 	return "Sem resposta!"
+	}
 }
 
 func main() {
-
+	winner := fastest("http://augustoscher.com", "https://www.google.com.br", "https://amazon.com")
+	fmt.Println(winner)
 }
