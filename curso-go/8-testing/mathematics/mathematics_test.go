f8c524414f40dbc7ba5f45718e06c9cfbb449d6e
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-12-08 13:30:38 -0300

    Adding simple test to Average function

---
+++ curso-go/8-testing/mathematics/mathematics_test.go
@@ -0,0 +1,15 @@
+package mathematics
+
+import "testing"
+
+const defaultError = "Expected: %v - Current was %v."
+
+//Test method has the same signature: Test + NameOfFunction
+func TestAverage(t *testing.T) {
+	expected := 7.28
+	value := Average(7.2, 9.9, 6.1, 5.9)
+
+	if value != expected {
+		t.Errorf(defaultError, expected, value)
+	}
+}
