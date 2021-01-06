218e619ddae7051760f69ac8d98d90076296839c
Augusto <augusto.scher@resultadosdigitais.com.br>
2021-01-06 09:59:01 -0300

    9-Database - select

--- curso-go/9-database/select/select.go
+++ curso-go/9-database/select/select.go
@@ -8,6 +8,11 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+type user struct {
+	id   int
+	name string
+}
+
 func main() {
 	db, error := sql.Open("mysql", "root:root@tcp(172.17.0.2:3306)/test")
 	if error != nil {
@@ -15,5 +20,20 @@ func main() {
 	}
 	fmt.Println("Connection opened successfully!")
 	defer db.Close()
+
+	rows, _ := db.Query("select id, name from users where id > ?", 1)
+	defer rows.Close()
+
+	var slice []user
+
+	for rows.Next() {
+		var u user
+		rows.Scan(&u.id, &u.name)
+		fmt.Println(u)
+		slice = append(slice, u)
+	}
+
+	fmt.Println(slice)
+	fmt.Println(slice[0])
 
 }
